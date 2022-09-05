a972b56a26059646694e1acc3d72157b84b0f16c
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-09-04 21:46:50 -0300

    :sparkles: feat: The header section component

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="relative bg-white overflow-hidden">
+  <div class="relative bg-gray-800 overflow-hidden">
     <div class="max-w-7xl mx-auto">
       <div
-        class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+        class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 bg-gradient-to-l from-indigo-900 to-gray-800"
       >
         <svg
           class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
@@ -11,7 +11,7 @@
           preserveAspectRatio="none"
           aria-hidden="true"
         >
-          <polygon points="50,0 100,0 50,100 0,100" />
+          <polygon class="text-indigo-900" points="50,0 100,0 50,100 50,100" />
         </svg>
 
         <Popover>
@@ -28,8 +28,8 @@
                     <span class="sr-only">Portal do trader</span>
                     <img
                       alt="Workflow"
-                      class="h-8 w-auto sm:h-10"
-                      src="https://res.cloudinary.com/portal-do-trader/image/upload/v1513012105/live/bs4-navbar-logo_p4tvfr.png"
+                      class="h-8 w-auto sm:h-14 sm:w-80"
+                      src="https://res.cloudinary.com/portal-do-trader/image/upload/v1602874706/pdt-home/logo_horiz_branco_full.png"
                     />
                   </a>
                   <div class="-mr-2 flex items-center md:hidden">
@@ -47,12 +47,12 @@
                   v-for="item in navigation"
                   :key="item.name"
                   :href="item.href"
-                  class="font-medium text-gray-500 hover:text-gray-900"
+                  class="font-medium text-gray-200 hover:text-gray-400"
                   >{{ item.name }}</a
                 >
                 <a
                   href="#"
-                  class="font-medium text-indigo-600 hover:text-indigo-500"
+                  class="font-medium text-emerald-400 hover:text-emerald-500"
                   @click="activeSignInModal"
                   >Entrar</a
                 >
@@ -115,20 +115,20 @@
           class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
         >
         <signin-form :class="{hidden: signinModal}"></signin-form>
-          <div class="sm:text-center lg:text-left">
+          <div class="sm:text-center lg:text-left text-gray-200">
             <h1
-              class="text-4xl tracking-tight font-bold text-gray-900 sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight"
+              class="text-4xl tracking-tight sm:text-5xl sm:tracking-tight md:text-6xl md:tracking-tight"
             >
               <span class="block xl:inline">Onde</span>
               {{ " " }}
-              <span class="block text-indigo-600 xl:inline"
+              <span class="block text-emerald-400 xl:inline font-bold"
                 >traders consistentes</span
               >
               <span class="block xl:inline"> se encontram</span>
               {{ " " }}
             </h1>
             <p
-              class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+              class="mt-3 text-base sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
             >
               Estudamos mais de 300 mil traders para criar um sistema
               educacional completo, que te ensina a melhorar seus resultados e a
@@ -140,7 +140,7 @@
               <div class="rounded-md shadow">
                 <a
                   href="#"
-                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-emerald-500 md:py-4 md:text-lg md:px-10 border-solid border-emerald-400 border-2 duration-75"
                 >
                   Registrar-se
                 </a>
@@ -148,7 +148,7 @@
               <div class="mt-3 sm:mt-0 sm:ml-3">
                 <a
                   href="#"
-                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-emerald-500 md:py-4 md:text-lg md:px-10 border-solid border-emerald-400 border-2 duration-75"
                 >
                   Cursos Gratuitos
                 </a>
