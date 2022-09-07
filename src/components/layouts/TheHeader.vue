686bf6bf4e547c27ceefe1e38860a65309247587
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-09-07 17:03:59 -0300

    :sparkles: feat: header component

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="relative bg-gray-800 overflow-hidden">
+  <div class="relative bg-slate-900 overflow-hidden">
     <div class="max-w-7xl mx-auto">
       <div
-        class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 bg-gradient-to-l from-indigo-900 to-gray-800"
+        class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 bg-gradient-to-r from-slate-900 to-blue-900"
       >
         <svg
           class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
@@ -11,7 +11,7 @@
           preserveAspectRatio="none"
           aria-hidden="true"
         >
-          <polygon class="text-indigo-900" points="50,0 100,0 50,100 50,100" />
+          <polygon class="text-blue-900" points="50,0 100,0 50,100 50,100" />
         </svg>
 
         <Popover>
