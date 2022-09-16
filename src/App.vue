c2d701c34f6c9581b56235796ab4de38cd32cf4d
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-09-16 11:45:46 -0300

    :construction: feat: ctatestimony component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <the-header></the-header>
   <intro-component></intro-component>
-  <cta-container></cta-container>
+  <cta-testimony :title="cta[0].title" :subTitle="cta[0].subTitle" :description="cta[0].description" :image="cta[0].image"></cta-testimony>
+  <cta-testimony :title="cta[1].title" :subTitle="cta[1].subTitle" :description="cta[1].description" :image="cta[1].image" :direction="cta[1].direction"></cta-testimony>
   <list-content></list-content>
   <team-container></team-container>
   <cta-component></cta-component>
@@ -23,20 +24,41 @@ import CourseContainer from '@/components/layouts/CourseContainer.vue'
 import TeamContainer from '@/components/layouts/TeamContainer.vue'
 import BlogContainer from '@/components/layouts/BlogContainer.vue'
 import CtaComponent from '@/components/layouts/CtaComponent.vue'
-import CtaContainer from '@/components/sections/CtaContainer.vue'
+import CtaTestimony from '@/components/sections/CtaTestimony.vue'
 
 export default {
   components: {
     TheHeader,
     IntroComponent,
     ListContent,
-    CtaContainer,
+    CtaTestimony,
     TheFooter,
     FaqContainer,
     CourseContainer,
     TeamContainer,
     BlogContainer,
     CtaComponent,
+  },
+  data() {
+    return {
+      cta: [
+        {
+          id: 0,
+          title: 'Análise Técnica, Fundamentalista e Tape Reading',
+          subTitle: 'Aprenda as melhores técnicas',
+          description: 'Seja para operar Day Trade, Swing Trade ou investir a longo prazo, nosso time de traders ensina as principais técnicas utilizadas no mercado como Price Action, Análise Técnica e Tape Reading em cursos gratuitos completos. Se você está começando agora, ou já é um trader experiente, será mais que bem-vindo para aprender o que quiser sobre trading e Bolsa de Valores.',
+          image: 'https://res.cloudinary.com/portal-do-trader/image/fetch/v1637071331/https://res.cloudinary.com/portal-do-trader/image/upload/f_webp%2Cfl_awebp%2Ct_ghost-blog-image/v1/blog-images/2021/11/o-que-e-mesa-proprietaria.jpg'
+        },
+        {
+          id: 1,
+          title: 'Aprenda a manejar o seu capital',
+          subTitle: 'Sem plano sem chance',
+          description: 'Não existe uma fórmula mágica para começar a operar com segurança nem para conseguir resultados positivos consistentes, o único caminho é através do conhecimento. Nossos traders profissionais estruturaram toda a jornada de aprendizado em um plano, com todas as etapas que você precisa passar para conquistar seus objetivos no mercado.',
+          image: 'https://res.cloudinary.com/portal-do-trader/image/fetch/v1643639789/https://res.cloudinary.com/portal-do-trader/image/upload/f_webp%2Ct_ghost-blog-image/v1/blog-images/2021/05/acoes.jpg',
+          direction: 'left'
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/sections/CtaTestimony.vue
@@ -0,0 +1,138 @@
+<template>
+  <div v-if="direction === 'right'">
+    <div class="sm:flex bg-gray-100">
+      <div class="relative md:w-1/2">
+        <div
+          class="h-56 sm:h-72 md:absolute md:left-0 md:h-full"
+        >
+          <img
+            class="w-full h-full object-cover"
+            :src="image"
+            alt="O portal do trader."
+          />
+        </div>
+      </div>
+      <div class="relative px-4 md:w-1/2 py-12 lg:px-8 lg:py-16">
+        <div class="md:pl-10 2xl:w-8/12">
+          <div class="md:ml-8">
+            <h2 class="text-lg font-semibold text-blue-900">
+              {{ title }}
+            </h2>
+            <p
+              class="mt-2 text-blue-900 text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight"
+            >
+              {{ subTitle }}
+            </p>
+            <p class="mt-3 text-xl text-gray-800">
+              {{ description }}
+            </p>
+            <testimony-card :studantPhoto="studant.photo" :studantName="studant.name" :testimony="studant.testimony"></testimony-card>
+            <div class="mt-8">
+              <div class="inline-flex rounded-md shadow">
+                <a
+                  href="#"
+                  class="inline-flex items-center justify-center px-5 py-3 border-2 border-gray-700 bg-blue-900 text-base font-medium rounded-md text-gray-200 hover:bg-gray-800 hover:border-indigo-600 duration-300"
+                >
+                  Visite nossa sala ao vivo
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else-if="direction === 'left'">
+    <div class="sm:flex bg-gray-100">
+      <div class="relative md:w-1/2 order-2">
+        <div
+          class="h-56 sm:h-72 md:absolute md:left-0 md:h-full"
+        >
+          <img
+            class="w-full h-full object-cover"
+            :src="image"
+            alt="O portal do trader."
+          />
+        </div>
+      </div>
+      <div class="relative px-4 md:w-1/2 py-12 lg:px-8 lg:py-16 flex justify-end">
+        <div class="md:pr-10 2xl:w-8/12 text-end">
+          <div class="md:mr-8">
+            <h2 class="text-lg font-semibold text-blue-900">
+              {{ title }}
+            </h2>
+            <p
+              class="mt-2 text-blue-900 text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight"
+            >
+              {{ subTitle }}
+            </p>
+            <p class="mt-3 text-xl text-gray-800">
+              {{ description }}
+            </p>
+            <testimony-card :studantPhoto="studant.photo" :studantName="studant.name" :testimony="studant.testimony"></testimony-card>
+            <div class="mt-8">
+              <div class="inline-flex rounded-md shadow">
+                <a
+                  href="#"
+                  class="inline-flex items-center justify-center px-5 py-3 border-2 border-gray-700 bg-blue-900 text-base font-medium rounded-md text-gray-200 hover:bg-gray-800 hover:border-indigo-600 duration-300"
+                >
+                  Visite nossa sala ao vivo
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ExternalLinkIcon } from "@heroicons/vue/solid";
+import TestimonyCard from "@/components/cards/TestimonyCard.vue"
+
+export default {
+  components: {
+    TestimonyCard
+  },
+  props: {
+    title: {
+      type: String,
+      require: true,
+      default: 'Title'
+    },
+    subTitle: {
+      type: String,
+      require: true,
+      default: 'Subtitle'
+    },
+    description: {
+      type: String,
+      require: true,
+      default: 'Insert the paragraph'
+    },
+    image: {
+      type: String,
+      require: true,
+      default: 'Put the image here'
+    },
+    direction: {
+      type: String,
+      require: true,
+      default: 'right',
+      validator: function(value) {
+        return value === 'right' || value === 'left'
+      }
+    }
+  },
+  data() {
+    return {
+      studant: {
+        name: 'Lucas A.',
+        photo: 'https://res.cloudinary.com/portal-do-trader/image/upload/f_auto/pdt-home/testimonial-pic-02',
+        testimony: 'Os cursos do Portal e a sala Ao Vivo mudaram minha vida operacional. A calma e paciência da equipe foram fundamentais para eu atingir minha consistência. Já conhecia o mercado e tinha perdido muito! Com vocês isso mudou! Não os conheço pessoalmente mas sinto como se fossem grandes amigos... Obrigado por tudo!'
+      },
+    }
+  },
+}
+</script>
