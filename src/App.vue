14cbab0d4f95493c2962cfa98aa2ed175c9c5fb9
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-09-16 14:33:40 -0300

    :construction: feat: Features Advanced component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <the-header></the-header>
   <intro-component></intro-component>
-  <cta-testimony :title="cta[0].title" :subTitle="cta[0].subTitle" :description="cta[0].description" :image="cta[0].image"></cta-testimony>
-  <cta-testimony :title="cta[1].title" :subTitle="cta[1].subTitle" :description="cta[1].description" :image="cta[1].image" :direction="cta[1].direction"></cta-testimony>
+  <cta-testimony
+    :title="cta[0].title"
+    :subTitle="cta[0].subTitle"
+    :description="cta[0].description"
+    :image="cta[0].image"
+  ></cta-testimony>
+  <cta-testimony
+    :title="cta[1].title"
+    :subTitle="cta[1].subTitle"
+    :description="cta[1].description"
+    :image="cta[1].image"
+    :direction="cta[1].direction"
+  ></cta-testimony>
   <list-content></list-content>
   <feature-list></feature-list>
   <team-container></team-container>
   <cta-component></cta-component>
   <course-container></course-container>
+  <features-advanced></features-advanced>
   <blog-container></blog-container>
   <signup-section></signup-section>
   <faq-container></faq-container>
@@ -16,17 +28,18 @@
 </template>
 
 <script>
-import TheHeader from '@/components/layouts/TheHeader.vue'
-import IntroComponent from '@/components/layouts/IntroComponent.vue'
-import ListContent from '@/components/layouts/ListContent.vue'
-import TheFooter from '@/components/layouts/TheFooter.vue'
-import FaqContainer from '@/components/layouts/FaqContainer.vue'
-import CourseContainer from '@/components/layouts/CourseContainer.vue'
-import TeamContainer from '@/components/layouts/TeamContainer.vue'
-import BlogContainer from '@/components/layouts/BlogContainer.vue'
-import CtaComponent from '@/components/layouts/CtaComponent.vue'
-import CtaTestimony from '@/components/sections/CtaTestimony.vue'
-import FeatureList from '@/components/layouts/FeatureList.vue'
+import TheHeader from "@/components/layouts/TheHeader.vue";
+import IntroComponent from "@/components/layouts/IntroComponent.vue";
+import ListContent from "@/components/layouts/ListContent.vue";
+import TheFooter from "@/components/layouts/TheFooter.vue";
+import FaqContainer from "@/components/layouts/FaqContainer.vue";
+import CourseContainer from "@/components/layouts/CourseContainer.vue";
+import TeamContainer from "@/components/layouts/TeamContainer.vue";
+import BlogContainer from "@/components/layouts/BlogContainer.vue";
+import CtaComponent from "@/components/layouts/CtaComponent.vue";
+import CtaTestimony from "@/components/sections/CtaTestimony.vue";
+import FeatureList from "@/components/layouts/FeatureList.vue";
+import FeaturesAdvanced from "@/components/layouts/FeaturesAdvanced.vue";
 
 export default {
   components: {
@@ -40,7 +53,8 @@ export default {
     TeamContainer,
     BlogContainer,
     CtaComponent,
-    FeatureList
+    FeaturesAdvanced,
+    FeatureList,
   },
   data() {
     return {
@@ -48,21 +62,25 @@ export default {
       cta: [
         {
           id: 0,
-          title: 'Análise Técnica, Fundamentalista e Tape Reading',
-          subTitle: 'Aprenda as melhores técnicas',
-          description: 'Seja para operar Day Trade, Swing Trade ou investir a longo prazo, nosso time de traders ensina as principais técnicas utilizadas no mercado como Price Action, Análise Técnica e Tape Reading em cursos gratuitos completos. Se você está começando agora, ou já é um trader experiente, será mais que bem-vindo para aprender o que quiser sobre trading e Bolsa de Valores.',
-          image: 'https://res.cloudinary.com/portal-do-trader/image/fetch/v1637071331/https://res.cloudinary.com/portal-do-trader/image/upload/f_webp%2Cfl_awebp%2Ct_ghost-blog-image/v1/blog-images/2021/11/o-que-e-mesa-proprietaria.jpg'
+          title: "Análise Técnica, Fundamentalista e Tape Reading",
+          subTitle: "Aprenda as melhores técnicas",
+          description:
+            "Seja para operar Day Trade, Swing Trade ou investir a longo prazo, nosso time de traders ensina as principais técnicas utilizadas no mercado como Price Action, Análise Técnica e Tape Reading em cursos gratuitos completos. Se você está começando agora, ou já é um trader experiente, será mais que bem-vindo para aprender o que quiser sobre trading e Bolsa de Valores.",
+          image:
+            "https://res.cloudinary.com/portal-do-trader/image/fetch/v1637071331/https://res.cloudinary.com/portal-do-trader/image/upload/f_webp%2Cfl_awebp%2Ct_ghost-blog-image/v1/blog-images/2021/11/o-que-e-mesa-proprietaria.jpg",
         },
         {
           id: 1,
-          title: 'Aprenda a manejar o seu capital',
-          subTitle: 'Sem plano sem chance',
-          description: 'Não existe uma fórmula mágica para começar a operar com segurança nem para conseguir resultados positivos consistentes, o único caminho é através do conhecimento. Nossos traders profissionais estruturaram toda a jornada de aprendizado em um plano, com todas as etapas que você precisa passar para conquistar seus objetivos no mercado.',
-          image: 'https://res.cloudinary.com/portal-do-trader/image/fetch/v1643639789/https://res.cloudinary.com/portal-do-trader/image/upload/f_webp%2Ct_ghost-blog-image/v1/blog-images/2021/05/acoes.jpg',
-          direction: 'left'
-        }
-      ]
-    }
-  }
-}
+          title: "Aprenda a manejar o seu capital",
+          subTitle: "Sem plano sem chance",
+          description:
+            "Não existe uma fórmula mágica para começar a operar com segurança nem para conseguir resultados positivos consistentes, o único caminho é através do conhecimento. Nossos traders profissionais estruturaram toda a jornada de aprendizado em um plano, com todas as etapas que você precisa passar para conquistar seus objetivos no mercado.",
+          image:
+            "https://res.cloudinary.com/portal-do-trader/image/fetch/v1643639789/https://res.cloudinary.com/portal-do-trader/image/upload/f_webp%2Ct_ghost-blog-image/v1/blog-images/2021/05/acoes.jpg",
+          direction: "left",
+        },
+      ],
+    };
+  },
+};
 </script>
---
+++ src/components/layouts/FeaturesAdvanced.vue
@@ -0,0 +1,71 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="relative bg-white py-16 sm:py-24 lg:py-32">
+    <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 class="text-lg font-semibold text-blue-900">6 anos no mercado</h2>
+      <p class="mt-2 text-3xl font-bold tracking-tight text-blue-900 sm:text-4xl">Consolidados com muita experiência</p>
+      <p class="mx-auto mt-5 max-w-prose text-xl text-gray-900">entenda por que acreditamos que juntos somos mais fortes</p>
+      <div class="mt-12">
+        <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+          <div v-for="feature in features" :key="feature.name" class="pt-6">
+            <div class="flow-root rounded-lg bg-gray-50 px-6 pb-8">
+              <div class="-mt-6">
+                <div>
+                  <span class="inline-flex items-center justify-center rounded-md bg-blue-900 p-3 shadow-lg">
+                    <component :is="feature.icon" class="h-6 w-6 text-emerald-400" aria-hidden="true" />
+                  </span>
+                </div>
+                <h3 class="mt-8 text-lg font-medium tracking-tight text-blue-900">{{ feature.name }}</h3>
+                <p class="mt-5 text-base text-gray-900">{{ feature.description }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  AcademicCapIcon,
+  CogIcon,
+  LockClosedIcon,
+  ServerIcon,
+  ShieldCheckIcon,
+  UserGroupIcon,
+} from '@heroicons/vue/outline'
+
+const features = [
+  {
+    name: '+12 milhões ',
+    description: 'de visualizações no youtube',
+    icon: AcademicCapIcon,
+  },
+  {
+    name: '+6 milhões',
+    description: 'de mensagens na sala ao vivo',
+    icon: LockClosedIcon,
+  },
+  {
+    name: '+300 mil',
+    description: 'usuários cadastrados',
+    icon: UserGroupIcon
+  },
+  {
+    name: '+1 milhão',
+    description: 'de aulas gratuitas vistas no último ano',
+    icon: ShieldCheckIcon,
+  },
+  {
+    name: '+7 mil',
+    description: 'transmissões e aulas produzidas',
+    icon: CogIcon,
+  },
+  {
+    name: '1,5 milhão',
+    description: 'de atividades respondidas em nossa plataforma',
+    icon: ServerIcon,
+  },
+]
+</script>
